df6b40e501137d51b527da461b08f2896aa510b1
3_lab <dubarevsergej9@gmail.com>
2023-05-25 14:08:06 +0300

    1

--- lab_9.py
+++ lab_9.py
@@ -1,28 +1,35 @@
 import os
-from PIL import Image
-import csv
-print("9.1 and 9.2")
-datadir = "D:\mashichev\pick"
-processdir = os.path.join(datadir , "Обработано")
-
-ListFiles = os.listdir(datadir)
-
-if os.path.isdir(processdir) == False:
-    os.mkdir(processdir)
-count = 0
-for  file in ListFiles:
-    filename = os.path.join(datadir, file)
-
-    if os.path.isfile(filename) and file.endswith('.png') or file.endswith('.jpg'):
-        with Image.open(filename) as img:
-            img.load()
-            rotated_img = img.rotate(45)
-            rotated_img.save(os.path.join(processdir, file))
-            count +=1
-print("Изображений обработано: ", count)
-def d2():
-    print("9.3")
-    file_path ="D:\mashichev\qwer.csv"
+def p1():
+    from PIL import Image, ImageFilter
+    if not os.path.isdir("New_Image"):
+        os.mkdir("New_Image")
+    os.chdir("Image")
+    for filename in os.listdir():
+            print(filename)
+            with Image.open(filename) as i:
+                i.load()
+                b = i.filter(ImageFilter.FIND_EDGES)
+                b.show()
+                os.chdir("../New_Image")
+                b.save("New" + filename)
+                os.chdir('../Image')
+def p2():
+    from PIL import Image, ImageFilter
+    if not os.path.isdir("New_Image"):
+        os.mkdir("New_Image")
+    os.chdir("Image")
+    for filename in os.listdir():
+        if filename.endswith('.jpg') or filename.endswith('.png'):
+            print(filename)
+            with Image.open(filename) as i:
+                i.load()
+                b = i.filter(ImageFilter.FIND_EDGES)
+                b.show()
+                os.chdir("../New_Image")
+                b.save("New" + filename)
+                os.chdir('../Image')
+def p3():
+    file_path ="D:\аип\exel.csv"
     with open(file_path) as f:
         total_cost = 0
         print('Нужно купить: ')
@@ -35,4 +42,4 @@ def d2():
             total_cost += cost
             print(f'{product} - {quantity} шт за {prise} руб')
     print(f'Итоговая стоимость: {total_cost} руб')
-d2()
+p3()
